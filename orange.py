4083377a7c423d2edc54963f2ca4012fb6c88893
Aditya <aditya.kumar@iitgn.ac.in>
2025-01-20 16:39:54 +0530

    Indentation Added

--- orange.py
+++ orange.py
@@ -1,8 +1,8 @@
 Number = int(input("Enter how much you like oranges on a scale of 10:"))
 if(Number >= 6):
-print("You like Oranges")
+    print("You like Oranges")
 else:
-print("You don't like Oranges")
+    print("You don't like Oranges")
 
 Number = int(input("Enter a number to rate Bananas")
 if(Number >= 7):
